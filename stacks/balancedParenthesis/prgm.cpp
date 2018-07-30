6d96efe6602ff4237e90e74b753db90c55d55730
Nilansh Bansal <bansalnilansh@gmail.com>
2018-07-30 16:21:51 +0530

    balanced parenthesis stack q

---
+++ stacks/balancedParenthesis/prgm.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+bool checkParenthesis(string input){
+    stack<char> s;
+    char temp;
+    for(int i=0;i<input.size();i++){
+        if(input[i] == '{' || input[i] == '(' || input[i] == '[' ){
+            s.push(input[i]);
+        }
+        else if(input[i] == '}' || input[i] == ')' || input[i] == ']'){
+            temp = s.top();
+            if((temp == '{' && input[i] == '}') || (temp == '(' && input[i] == ')') || (temp == '[' && input[i] == ']')){
+                s.pop();
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    if(!s.empty()){
+        return false;
+    }
+
+    return true;
+}
+
+int main(){
+    string input;
+    cout<<"Enter expression : "<<endl;
+    getline(cin,input);
+
+    cout<<(checkParenthesis(input)?"BALANCED EXPRESSION!":"NOT BALANCED !");    
+
+
+    return 0;
+}
